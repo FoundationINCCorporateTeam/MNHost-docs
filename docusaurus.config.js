ac520136b539b4d89969679398e6fab7fc81c410
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-04-02 20:17:58 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,10 +96,6 @@ const config = {
             ],
           },
           {
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
